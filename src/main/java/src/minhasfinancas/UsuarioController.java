1356d2b47b1d4273755b86ec41503ef9ffcdb149
Izabella Marques <izamarquessoares@gmail.com>
2023-04-12 22:11:10 -0300

    versao funcional

--- src/main/java/src/minhasfinancas/UsuarioController.java
+++ src/main/java/src/minhasfinancas/UsuarioController.java
@@ -1,7 +1,16 @@
 package src.minhasfinancas;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class UsuarioController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "HELLO";
+    }
+
+
 }
